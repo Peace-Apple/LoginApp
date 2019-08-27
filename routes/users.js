6cdfb361bd8d632a7cd7a5fa8ebb4b359f98ed7d
peace <astonreba@gmail.com>
2019-08-27 15:23:49 +0300

    Modify files

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,41 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
+import models from '../models';
 
 const router = express.Router();
 
 // Register
-router.get('/register', (req, res) => {
-  res.render('register');
+router.post('/register', (req, res) => {
+  const { name, username, email, password, password2 } = req.body;
+
+  // validation
+//   req.checkBody('name', 'Name is required').notEmpty();
+//   req.checkBody('username', 'Username is required').notEmpty();
+//   req.checkBody('email', 'Email is required').notEmpty();
+//   req.checkBody('email', 'Email is not valid').isEmail();
+//   req.checkBody('password', 'Password is required').notEmpty();
+//   req.checkBody('password2', 'Passwords do not match').equals(password);
+
+//   const errors = req.validationErrors();
+//   if(errors) {
+//     res.render('register', {
+//         errors: errors
+//     })
+//   } else {
+    bcrypt.hash(password, 10, async(err, hash) => {
+      const newUser = await models.User.create({
+        name: name,
+        username: username,
+        email: email,
+        password: hash
+      });
+      console.log(newUser);
+    });
+//   }
 });
 
 // Login
-router.post('/login', (req, res) => {
+router.get('/login', (req, res) => {
   res.render('login');
 })
 
