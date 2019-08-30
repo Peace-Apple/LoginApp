2d604b576a0b702705fcdefcfd188bbe18581cd7
peace <astonreba@gmail.com>
2019-08-30 12:20:57 +0300

    Modify file

--- app.js
+++ app.js
@@ -70,10 +70,11 @@ app.use(passport.session());
 app.use(flash());
 
 //global vars
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import models from '../models';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
 
 const router = express.Router();
 
 // Register
+router.get('/register', (req, res) => {
+  res.render('register');
+})
+
+// Login
+router.get('/login', (req, res) => {
+  res.render('login');
+})
+
+// Register User
 router.post('/register', (req, res) => {
   const { name, username, email, password, password2 } = req.body;
 
@@ -29,14 +41,61 @@ router.post('/register', (req, res) => {
         email: email,
         password: hash
       });
-      console.log(newUser);
+      if (newUser) {
+        req.flash('success_msg', 'You have successfully registered and can now log in')
+        res.redirect('/users/login')
+      }
     });
 //   }
 });
 
-// Login
-router.get('/login', (req, res) => {
-  res.render('login');
-})
+passport.use(new LocalStrategy(
+  async(username, password, done) => {
+    const user = await models.User.findOne({
+      where: {
+        username
+      }
+    });
+
+    if(!user) {
+      return done(null, false, {
+        message: 'Unknown user'
+      })
+    };
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) {
+      return done(null, false, {
+        message: 'Password is incorrect, please enter the right one!'
+      })
+    }
+    return done(null, user);
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async(id, done) => {
+  const user = await models.User.findByPk(id);
+  done(null, user);
+});
+
+// Login user 
+router.post('/login',
+  passport.authenticate('local', { successRedirect: '/', 
+    failureRedirect: '/users/login', failureFlash: true }),
+  (req, res) => {
+    res.redirect('/');
+});
+
+// Logout user
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
 
 module.exports = router;
+ 
